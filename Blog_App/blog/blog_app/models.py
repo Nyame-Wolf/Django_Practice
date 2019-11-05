1b36c14409663424ca1512c5440c236739261a02
Nyame-Wolf <mumenyam@gmail>
2019-11-05 05:26:07 +0300

    DB operations; creating a model

--- Blog_App/blog/blog_app/models.py
+++ Blog_App/blog/blog_app/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
  
 class User(AbstractUser):
      pass
+
+class Post(models.Model):
+     title = models.CharField(max_length=100)
+     content = models.TextField()
+     date_posted = models.DateTimeField(default=timezone.now)
+     author = models.ForeignKey(User, on_delete=models.CASCADE)
+     
