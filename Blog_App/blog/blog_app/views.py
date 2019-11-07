6f5f178f128770335eb08c3eb168cd69033cd238
Nyame-Wolf <mumenyam@gmail>
2019-11-07 10:26:37 +0300

    Making queries to the db, and displayimh them on home page

--- Blog_App/blog/blog_app/views.py
+++ Blog_App/blog/blog_app/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-
+from .models import Post
 
 def home(request):
-    return render(request,'blog_app/home.html')
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request,'blog_app/home.html',context)
+
 def about(request):
-    return render(request,'blog_app/about.html')
+    return render(request,'blog_app/about.html', {'title':'About'})
