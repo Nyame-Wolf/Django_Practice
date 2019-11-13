81827018c4f25f73b3b14cf27d42231103ae4b73
Nyame-Wolf <mumenyam@gmail>
2019-11-13 10:17:47 +0300

    Adding information to see if data is valid-what we expected

--- Blog_App/blog/users/views.py
+++ Blog_App/blog/users/views.py
@@ -4,6 +4,8 @@ from django.contrib.auth.forms import UserCreationForm
 def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
     else:
         form = UserCreationForm()
     return render(request, 'users/register.html', {'form':form})
