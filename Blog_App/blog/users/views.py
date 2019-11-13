38ce82447585338057f3de50b6978871a186c739
Nyame-Wolf <mumenyam@gmail>
2019-11-13 10:09:56 +0300

    Adding information from our forms

--- Blog_App/blog/users/views.py
+++ Blog_App/blog/users/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 
 def register(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+    else:
+        form = UserCreationForm()
     return render(request, 'users/register.html', {'form':form})
